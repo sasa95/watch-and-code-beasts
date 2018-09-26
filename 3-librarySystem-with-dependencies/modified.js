49b535bc1ec906670dfda0fa73876f63c34a8a33
Sasa Drmic <sasa.drmic@riitech.com>
2018-09-26 22:39:24 +0200

    Solved beast #3

--- 3-librarySystem-with-dependencies/modified.js
+++ 3-librarySystem-with-dependencies/modified.js
@@ -3,21 +3,26 @@
 
   function librarySystem(libraryName, dependecies, callback) {
     if (arguments.length > 1) {
-      if (Array.isArray(dependecies) && dependecies.length > 0) {
-        // do something
-        libraryStorage[libraryName] = callback.call(null, libraryStorage[dependecies[0]]);
-
+      if (Array.isArray(dependecies)) {
+        if (dependecies.length) {
+          var libs = dependecies.map(function (element) {
+            return libraryStorage[element]
+          })
+          libraryStorage[libraryName] = callback.apply(null, libs);
+        } else {
+          libraryStorage[libraryName] = callback();
+        }
       } else {
-        libraryStorage[libraryName] = callback();
+        throw new Error('Second argument of librarySystem function should be an array.')
       }
     } else {
       return libraryStorage[libraryName];
     }
   }
-
   window.librarySystem = librarySystem;
 }());
 
+// Test 1
 librarySystem('dependency', [], function () {
   return 'loaded dependency';
 });
@@ -26,5 +31,19 @@ librarySystem('app', ['dependency'], function (dependency) {
   return 'app with ' + dependency;
 });
 
-console.log(librarySystem('dependency')); // 'loaded dependency'
 console.log(librarySystem('app')); // 'app with loaded dependency'
+
+// Test 2
+librarySystem('name', [], function () {
+  return 'Gordon';
+});
+
+librarySystem('company', [], function () {
+  return 'Watch and Code';
+});
+
+librarySystem('workBlurb', ['name', 'company'], function (name, company) {
+  return name + ' works at ' + company;
+});
+
+console.log(librarySystem('workBlurb')); // 'Gordon works at Watch and Code'
