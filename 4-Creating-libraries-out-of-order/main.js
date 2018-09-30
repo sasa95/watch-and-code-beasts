1fff29f010d2349ea9664a8a6690ffe03edd45b1
Sasa Drmic <sasa.drmic@riitech.com>
2018-09-30 16:10:28 +0200

    Solved #4

--- 4-Creating-libraries-out-of-order/main.js
+++ 4-Creating-libraries-out-of-order/main.js
@@ -3,83 +3,58 @@
   var librariesOnHold = [];
 
   function librarySystem(libraryName, dependecies, callback) {
-    debugger;
     if (arguments.length > 1) {
       if (Array.isArray(dependecies)) {
-        // If there are some dependencies provided
         if (dependecies.length) {
           var dependeciesMissing = false;
-          dependeciesMissing = dependecies.some(function(element) {
+          dependeciesMissing = dependecies.some(function (element) {
             return libraryStorage[element] === undefined;
           });
 
           if (dependeciesMissing) {
-            librariesOnHold.push({libraryName: libraryName, dependecies: dependecies, callback: callback});
+            librariesOnHold.push({ libraryName: libraryName, dependecies: dependecies, callback: callback });
           } else {
-            var libs = dependecies.map(function(element) {
+            var libs = dependecies.map(function (element) {
               return libraryStorage[element]
             });
             libraryStorage[libraryName] = callback.apply(null, libs);
-            
           }
-
         } else {
-          // If there are no dependencies provided
           libraryStorage[libraryName] = callback();
         }
-
       } else {
         throw new Error('Second argument of librarySystem function should be an array.')
       }
     } else {
-      var createLib = false;
-      var libToCreate = null;
-
-      createLib = librariesOnHold.some(function(element, index) {
-        if (element.libraryName === libraryName) {
-          libToCreate = element;
-          librariesOnHold.splice(index, 1);
-          return true;
-        }
-      });
-
-      if (createLib) {
-        var deps = libToCreate.dependecies.map(function(element) {
-          return libraryStorage[element]
+      if (librariesOnHold.length > 0) {
+        var createLib = false;
+        var libToCreate = null;
+  
+        createLib = librariesOnHold.some(function (element, index) {
+          if (element.libraryName === libraryName) {
+            libToCreate = element;
+            librariesOnHold.splice(index, 1);
+            return true;
+          }
         });
-        libraryStorage[libraryName] = libToCreate.callback.apply(null, deps);
-        return libraryStorage[libraryName];
-      } else {
-        return libraryStorage[libraryName];
+        var depsMissing = 0;
+        if (createLib) {
+          var deps = libToCreate.dependecies.map(function (element) {
+            if (!libraryStorage[element]) {
+              depsMissing ++;
+            }
+            return libraryStorage[element]
+          });
+          
+          if (depsMissing) {
+            libraryStorage[libraryName] = undefined;
+          } else {
+            libraryStorage[libraryName] = libToCreate.callback.apply(null, deps);
+          }
+        }
       }
+      return libraryStorage[libraryName];
     }
   }
   window.librarySystem = librarySystem;
 }());
-
-
-librarySystem('workBlurb', ['name', 'company'], function (name, company) {
-  return name + ' works at ' + company;
-}); 
-
-librarySystem('name', [], function () {
-  return 'Gordon';
-});
-
-librarySystem('app', ['dependency'], function(dependency) {
-  return 'app with ' + dependency;
-});
-
-librarySystem('company', [], function () {
-  return 'Watch and Code';
-});
-
-librarySystem('dependency', [], function() {
-  return 'loaded dependency';
-});
-
-
-
-console.log(librarySystem('app')); // 'app with loaded dependency'
-
-console.log(librarySystem('workBlurb')); // 'Gordon works at Watch and Code'
